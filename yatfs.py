38b4b26637757710e1c1469b62af0a4066333401
denys <denys@denys-pc>
2011-03-11 14:16:28 +0200

    Method renaming

--- yatfs.py
+++ yatfs.py
@@ -1,100 +1,42 @@
-from fuse import Fuse
+#!/usr/bin/env python
+import fuse
 
-class Yatfs(Fuse):
+fuse.fuse_python_api = (0, 2)
+
+class YatFS(fuse.Fuse):
     """
     """
 
     def __init__(self, *args, **kw):
-        Fuse.__init__(self, *args, **kw)
-        print 'Init complete.'
-
-    def getattr(self, path):
-        print '*** getattr', path
-        return -errno.ENOSYS
-
-    def getdir(self, path):
-        print '*** getdir', path
-        return -errno.ENOSYS
-
-    def mythread ( self ):
-        print '*** mythread'
-        return -errno.ENOSYS
-
-    def chmod ( self, path, mode ):
-        print '*** chmod', path, oct(mode)
-        return -errno.ENOSYS
-
-    def chown ( self, path, uid, gid ):
-        print '*** chown', path, uid, gid
-        return -errno.ENOSYS
-
-    def fsync ( self, path, isFsyncFile ):
-        print '*** fsync', path, isFsyncFile
-        return -errno.ENOSYS
-
-    def link ( self, targetPath, linkPath ):
-        print '*** link', targetPath, linkPath
-        return -errno.ENOSYS
-
-    def mkdir ( self, path, mode ):
-        print '*** mkdir', path, oct(mode)
-        return -errno.ENOSYS
-
-    def mknod ( self, path, mode, dev ):
-        print '*** mknod', path, oct(mode), dev
-        return -errno.ENOSYS
-
-    def open ( self, path, flags ):
-        print '*** open', path, flags
-        return -errno.ENOSYS
-
-    def read ( self, path, length, offset ):
-        print '*** read', path, length, offset
-        return -errno.ENOSYS
-
-    def readlink ( self, path ):
-        print '*** readlink', path
-        return -errno.ENOSYS
-
-    def release ( self, path, flags ):
-        print '*** release', path, flags
-        return -errno.ENOSYS
-
-    def rename ( self, oldPath, newPath ):
-        print '*** rename', oldPath, newPath
-        return -errno.ENOSYS
-
-    def rmdir ( self, path ):
-        print '*** rmdir', path
-        return -errno.ENOSYS
-
-    def statfs ( self ):
-        print '*** statfs'
-        return -errno.ENOSYS
-
-    def symlink ( self, targetPath, linkPath ):
-        print '*** symlink', targetPath, linkPath
-        return -errno.ENOSYS
-
-    def truncate ( self, path, size ):
-        print '*** truncate', path, size
-        return -errno.ENOSYS
-
-    def unlink ( self, path ):
-        print '*** unlink', path
-        return -errno.ENOSYS
-
-    def utime ( self, path, times ):
-        print '*** utime', path, times
-        return -errno.ENOSYS
-
-    def write ( self, path, buf, offset ):
-        print '*** write', path, buf, offset
-        return -errno.ENOSYS
+        fuse.Fuse.__init__(self, *args, **kw)
+        self.README = 'This is YatFS\n'
+        self.source_path = '/'
+    def readdir(self, path, offset):
+        # 
+        yield fuse.Direntry('.')
+        yield fuse.Direntry('..')
+        if path == '/':
+            # 
+            yield fuse.Direntry('README')
+            yield fuse.Direntry('simple')
 
 
-if __name__ == __main__:
-    fs = NullFS()
-    fs.flags = 0
-    fs.multithreaded = 0
-    fs.main()
+    
+def main():
+    usage = """
+        Yatfs
+
+    """ + fuse.Fuse.fusage
+
+  
+    server = YatFS(version="%prog " + fuse.__version__,
+                 usage=usage,
+                 dash_s_do='setsingle')
+
+    server.parser.add_option(mountopt="source", metavar="PATH", default='/',
+                             help="source folder")
+    server.parse(values=server, errex=1)
+    server.main()
+
+if __name__ == '__main__':
+    main()
